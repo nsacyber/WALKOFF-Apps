6ee18ba23f5e72f460ffa4a029cb69483f9bc70f
iadgovuser11 <iadgovuser11@users.noreply.github.com>
2017-07-12 12:03:33 -0400

    Apps migrated from Walkoff main repository

--- EthereumBlockchain/main.py
+++ EthereumBlockchain/main.py
@@ -1,4 +1,4 @@
-from server.appdevice import App
+from apps import App, action
 import os, subprocess, signal
 
 
@@ -10,39 +10,42 @@ class Main(App):
         self.totalNodes = "2"
         App.__init__(self, name, device)
 
-    def create_accounts(self, args={}):
+    @action
+    def create_accounts(self, total_nodes):
         res = 0
-        if args["total_nodes"] and args["total_nodes"] > 2:
-            self.totalNodes = str(args["total_nodes"])
+        self.totalNodes = total_nodes
 
         self.terminate_geth_processes()
-        res &= self.run_script(["step1-create-accounts.sh", self.totalNodes])
+        res &= self._run_script(["step1-create-accounts.sh", self.totalNodes])
         return res
 
-    def set_up_network(self, args={}):
+    @action
+    def set_up_network(self):
         res = 0
-        res &= self.run_script(["step2-create-genesis-file.sh"])
-        res &= self.run_script(["step3-start-miners.sh"])
-        res &= self.run_script(["step4-connect-miners.sh", self.totalNodes])
-        res &= self.run_script(["step5-deploy-contract.sh", self.totalNodes])
+        res &= self._run_script(["step2-create-genesis-file.sh"])
+        res &= self._run_script(["step3-start-miners.sh"])
+        res &= self._run_script(["step4-connect-miners.sh", self.totalNodes])
+        res &= self._run_script(["step5-deploy-contract.sh", self.totalNodes])
         return res
 
-    def submit_greeting(self, args={}):
-        if args["greeting"]:
-            self.greeting = args["greeting"]
-        return self.run_script(["step6-submit-greeting.sh", self.greeting])
+    @action
+    def submit_greeting(self, greeting):
+        self.greeting = greeting
+        return self._run_script(["step6-submit-greeting.sh", self.greeting])
 
-    def run_script(self, args=[]):
+    def _run_script(self, args=[]):
         args[0] = self.curDirPath + "/" + args[0] # Full path to the script file
         args.insert(1, self.curDirPath) # Full path to the Ethereum Blockchain directory
         process = subprocess.Popen(args)
         return process.wait()
 
-    def terminate_geth_processes(self, args={}):
-        processName = "geth"
-        psCmd = subprocess.Popen("ps -A", shell=True, stdout=subprocess.PIPE)
-        grepCmd = subprocess.Popen("grep " + processName, shell=True, stdin=psCmd.stdout, stdout=subprocess.PIPE)
-        out, err = grepCmd.communicate()
+    @action
+    def terminate_geth_processes(self):
+        process_name = "geth"
+        ps_cmd = subprocess.Popen("ps -A", shell=True, stdout=subprocess.PIPE)
+        grep_cmd = subprocess.Popen("grep " + process_name, shell=True, stdin=ps_cmd.stdout, stdout=subprocess.PIPE)
+        out, err = grep_cmd.communicate()
         for line in out.splitlines():
             pid = int(line.split(None, 1)[0])
             os.kill(pid, signal.SIGKILL)
+        return "Success"
