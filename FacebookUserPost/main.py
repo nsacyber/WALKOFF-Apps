00bc3e688120dcf5092b8cba4d4daad68100198c
Justin Tervala <Tervala_Justin@bah.com>
2017-10-26 13:33:14 -0400

    Modified YAMLS to fit with latest spec

--- FacebookUserPost/main.py
+++ FacebookUserPost/main.py
@@ -5,13 +5,13 @@ import requests
 class Main(App):
 
     def __init__(self, name=None, device=None):
-        self.user_id = self.get_device().username
-        self.user_access_token = self.get_device().get_password()
+        self.user_id = self.device_fields['username']
+        self.user_access_token = self.device.get_encrypted_field('password')
         App.__init__(self, name, device)
 
     @action
     def post_to_user_wall(self, message):
         msg = message.replace(" ", "+")
         url = ('https://graph.facebook.com/v2.9/' + self.user_id + '/feed?'
-               'message=' + msg + '&access_token=' + self.get_device().get_password())
+               'message=' + msg + '&access_token=' + self.device.get_encrypted_field('token'))
         return (requests.post(url, verify=False)).text
