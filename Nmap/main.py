828729574b1e0f2eb823a5aee758410bc6a2d18b
adpham95 <32873177+adpham95@users.noreply.github.com>
2017-11-29 13:08:50 -0500

    making some nmap actions global

--- Nmap/main.py
+++ Nmap/main.py
@@ -2,7 +2,139 @@ from apps import App, action
 from libnmap.process import NmapProcess
 from libnmap.parser import NmapParser
 from jinja2 import Environment
+import json
 import sys
+
+
+@action
+def run_scan(target, options):
+    nmap_proc = NmapProcess(target, options)
+    nmap_proc.run()
+    xx = nmap_proc.stdout
+    if sys.version_info[0] == 2:
+        xx = xx.encode('utf-8')
+    nmap_report = NmapParser.parse(nmap_data=xx, data_type='XML')
+    ret = [{'name': host.hostnames.pop() if len(host.hostnames) else host.address,
+            'address': host.address,
+            'services': [{'port': service.port,
+                          'protocol': service.protocol,
+                          'state': service.state,
+                          'service': service.service,
+                          'banner': service.banner} for service in host.services]}
+           for host in nmap_report.hosts]
+    return ret
+
+@action
+def ports_and_hosts_from_json(string, is_file=False):
+
+    try:
+        if is_file:
+            with open(string) as j:
+                obj = json.load(j)
+        else:
+            obj = json.loads(string)
+    except IOError:
+        return False, 'NotExists'
+    except (AttributeError, ValueError) as e:
+        return False, 'JSONError'
+
+    r = {"ports": [], "hosts": []}
+    for host in obj:
+        r["hosts"].append(host["address"])
+        for svc in host["services"]:
+            if svc["protocol"] == "tcp":
+                r["ports"].append("T:" + str(svc["port"]))
+            elif svc["protocol"] == "udp":
+                r["ports"].append("U:" + str(svc["port"]))
+
+    r["ports"] = ",".join(r["ports"])
+    r["hosts"] = ",".join(r["hosts"])
+
+    return r
+
+@action
+def scan_results_as_html(self, results):
+    html = '''
+    <table style="width:50%;border-collapse:collapse">
+            <tr>
+                <th style="background-color:#3c8dbc;color:#f2f2f2;text-align:center;vertical-align:bottom;">Name</th>
+                <th style="background-color:#3c8dbc;color:#f2f2f2;text-align:center;vertical-align:bottom;">Address</th>
+                <th style="background-color:#3c8dbc;color:#f2f2f2;text-align:center;vertical-align:bottom;">Port</th>
+                <th style="background-color:#3c8dbc;color:#f2f2f2;text-align:center;vertical-align:bottom;">Protocol</th>
+                <th style="background-color:#3c8dbc;color:#f2f2f2;text-align:center;vertical-align:bottom;">State</th>
+                <th style="background-color:#3c8dbc;color:#f2f2f2;text-align:center;vertical-align:bottom;">Service</th>
+                <th style="background-color:#3c8dbc;color:#f2f2f2;text-align:center;vertical-align:bottom;">Banner</th>
+            </tr>
+            {% for host in results %}
+                {% for service in host['services'] %}
+                    <tr>
+                        {%- if loop.index%2 == 0 -%}
+                            <td style="text-align: left;vertical-align: center;border-bottom: 1px solid #ddd;background-color: #cfd4d6;">{{ host['name'] }}</td>
+                            <td style="text-align: left;vertical-align: center;border-bottom: 1px solid #ddd;background-color: #cfd4d6;">{{ host['address'] }}</td>
+                            <td style="text-align: center;vertical-align: center;border-bottom: 1px solid #ddd;background-color: #cfd4d6;">{{ service['port'] }}</td>
+                            <td style="text-align: center;vertical-align: center;border-bottom: 1px solid #ddd;background-color: #cfd4d6;">{{ service['protocol'] }}</td>
+                            <td style="text-align: center;vertical-align: center;border-bottom: 1px solid #ddd;background-color: #cfd4d6;">{{ service['state'] }}</td>
+                            <td style="text-align: center;vertical-align: center;border-bottom: 1px solid #ddd;background-color: #cfd4d6;">{{ service['service'] }}</td>
+                            <td style="text-align: center;vertical-align: center;border-bottom: 1px solid #ddd;background-color: #cfd4d6;">{{ service['banner'] }}</td>
+                        {%- else -%}
+                            <td style="text-align: left;vertical-align: center;border-bottom: 1px solid #ddd;">{{ host['name'] }}</td>
+                            <td style="text-align: left;vertical-align: center;border-bottom: 1px solid #ddd;">{{ host['address'] }}</td>
+                            <td style="text-align: center;vertical-align: center;border-bottom: 1px solid #ddd;">{{ service['port'] }}</td>
+                            <td style="text-align: center;vertical-align: center;border-bottom: 1px solid #ddd;">{{ service['protocol'] }}</td>
+                            <td style="text-align: center;vertical-align: center;border-bottom: 1px solid #ddd;">{{ service['state'] }}</td>
+                            <td style="text-align: center;vertical-align: center;border-bottom: 1px solid #ddd;">{{ service['service'] }}</td>
+                            <td style="text-align: center;vertical-align: center;border-bottom: 1px solid #ddd;">{{ service['banner'] }}</td>
+                        {%- endif -%}
+                    </tr>
+                {% endfor %}
+          {% endfor %}
+        </table>
+    '''
+    return Environment().from_string(html).render(results=results)
+
+@action
+def hosts_from_json(string, is_file=False):
+
+    try:
+        if is_file:
+            with open(string) as j:
+                obj = json.load(j)
+        else:
+            obj = json.loads(string)
+    except IOError:
+        return False, 'NotExists'
+    except (AttributeError, ValueError) as e:
+        return False, 'JSONError'
+
+    hosts = []
+    for host in obj:
+        hosts.append(host["address"])
+
+    return ",".join(hosts)
+
+@action
+def ports_from_json(string, is_file=False):
+
+    try:
+        if is_file:
+            with open(string) as j:
+                obj = json.load(j)
+        else:
+            obj = json.loads(string)
+    except IOError:
+        return False, 'NotExists'
+    except (AttributeError, ValueError) as e:
+        return False, 'JSONError'
+
+    ports = []
+    for host in obj:
+        for svc in host["services"]:
+            if svc["protocol"] == "tcp":
+                ports.append("T:" + str(svc["port"]))
+            elif svc["protocol"] == "udp":
+                ports.append("U:" + str(svc["port"]))
+
+    return ",".join(ports)
 
 
 class Main(App):
@@ -26,65 +158,6 @@ class Main(App):
     @action
     def clear_blacklist(self):
         self.blacklist = []
-
-    @action
-    def run_scan(self, target, options):
-        nmap_proc = NmapProcess(target, options)
-        nmap_proc.run()
-        xx = nmap_proc.stdout
-        if sys.version_info[0] == 2:
-            xx = xx.encode('utf-8')
-        nmap_report = NmapParser.parse(nmap_data=xx, data_type='XML')
-        ret = [{'name': host.hostnames.pop() if len(host.hostnames) else host.address,
-                'address': host.address,
-                'services': [{'port': service.port,
-                              'protocol': service.protocol,
-                              'state': service.state,
-                              'service': service.service,
-                              'banner': service.banner} for service in host.services]}
-               for host in nmap_report.hosts]
-        return ret
-
-    @action
-    def scan_results_as_html(self, results):
-        html = '''
-        <table style="width:50%;border-collapse:collapse">
-                <tr>
-                    <th style="background-color:#3c8dbc;color:#f2f2f2;text-align:center;vertical-align:bottom;">Name</th>
-                    <th style="background-color:#3c8dbc;color:#f2f2f2;text-align:center;vertical-align:bottom;">Address</th>
-                    <th style="background-color:#3c8dbc;color:#f2f2f2;text-align:center;vertical-align:bottom;">Port</th>
-                    <th style="background-color:#3c8dbc;color:#f2f2f2;text-align:center;vertical-align:bottom;">Protocol</th>
-                    <th style="background-color:#3c8dbc;color:#f2f2f2;text-align:center;vertical-align:bottom;">State</th>
-                    <th style="background-color:#3c8dbc;color:#f2f2f2;text-align:center;vertical-align:bottom;">Service</th>
-                    <th style="background-color:#3c8dbc;color:#f2f2f2;text-align:center;vertical-align:bottom;">Banner</th>
-                </tr>
-                {% for host in results %}
-                    {% for service in host['services'] %}
-                        <tr>
-                            {%- if loop.index%2 == 0 -%}
-                                <td style="text-align: left;vertical-align: center;border-bottom: 1px solid #ddd;background-color: #cfd4d6;">{{ host['name'] }}</td>
-                                <td style="text-align: left;vertical-align: center;border-bottom: 1px solid #ddd;background-color: #cfd4d6;">{{ host['address'] }}</td>
-                                <td style="text-align: center;vertical-align: center;border-bottom: 1px solid #ddd;background-color: #cfd4d6;">{{ service['port'] }}</td>
-                                <td style="text-align: center;vertical-align: center;border-bottom: 1px solid #ddd;background-color: #cfd4d6;">{{ service['protocol'] }}</td>
-                                <td style="text-align: center;vertical-align: center;border-bottom: 1px solid #ddd;background-color: #cfd4d6;">{{ service['state'] }}</td>
-                                <td style="text-align: center;vertical-align: center;border-bottom: 1px solid #ddd;background-color: #cfd4d6;">{{ service['service'] }}</td>
-                                <td style="text-align: center;vertical-align: center;border-bottom: 1px solid #ddd;background-color: #cfd4d6;">{{ service['banner'] }}</td>
-                            {%- else -%}
-                                <td style="text-align: left;vertical-align: center;border-bottom: 1px solid #ddd;">{{ host['name'] }}</td>
-                                <td style="text-align: left;vertical-align: center;border-bottom: 1px solid #ddd;">{{ host['address'] }}</td>
-                                <td style="text-align: center;vertical-align: center;border-bottom: 1px solid #ddd;">{{ service['port'] }}</td>
-                                <td style="text-align: center;vertical-align: center;border-bottom: 1px solid #ddd;">{{ service['protocol'] }}</td>
-                                <td style="text-align: center;vertical-align: center;border-bottom: 1px solid #ddd;">{{ service['state'] }}</td>
-                                <td style="text-align: center;vertical-align: center;border-bottom: 1px solid #ddd;">{{ service['service'] }}</td>
-                                <td style="text-align: center;vertical-align: center;border-bottom: 1px solid #ddd;">{{ service['banner'] }}</td>
-                            {%- endif -%}
-                        </tr>
-                    {% endfor %}
-              {% endfor %}
-            </table>
-        '''
-        return Environment().from_string(html).render(results=results)
-
 
     @action
     def get_hosts_from_scan(self, target, options=''):
