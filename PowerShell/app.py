0a65dc484bd36c9d655856a3eccc36907fcf91dd
xomm <aatadp@gmail.com>
2018-03-08 13:21:38 -0500

    all three actions in powershell app now fully functional, kansa support

--- PowerShell/app.py
+++ PowerShell/app.py
@@ -1,10 +1,10 @@
 from apps import App, action
 import winrm
 import subprocess
-
+import chardet
 
 @action
-def exec_local_command(command, output_filename=None):
+def exec_local_command(commands, output_filename=None):
     """
     Use Powershell client to execute commands on the remote server and produce an array of command outputs
     Input:
@@ -14,9 +14,9 @@ def exec_local_command(command, output_filename=None):
     """
     results = []
     status = "Success"
-    for cmd in command:
+    for command in commands:
         try:
-            output = subprocess.check_output(["powershell.exe", cmd], shell=True)
+            output = subprocess.check_output(["powershell.exe", command], shell=True)
             results.append(output)
         except subprocess.CalledProcessError as e:
             results.append(e)
@@ -37,14 +37,19 @@ class PowerShell(App):
     def __init__(self, name=None, device=None):
         App.__init__(self, name, device)
         self.host = "{}:{}".format(self.device_fields["host"], self.device_fields["port"])
+        if self.device_fields["https"]:
+            self.host = "https://" + self.host
         self.username = self.device_fields["username"]
         self.insecure_mode = {}
         if self.device_fields["very_insecure_mode_testing_only"]:
             self.insecure_mode = {"transport": "ntlm", "server_cert_validation": "ignore"}
-        self.winrm = None
+        self.winrm = winrm.Session(self.host,
+                                   auth=(self.username,
+                                         self.device.get_encrypted_field("password")),
+                                   **self.insecure_mode)
 
     @action
-    def exec_remote_command(self, command, output_filename=None):
+    def exec_remote_command(self, commands, output_filename=None):
 
         """
         Use Powershell client to execute commands on the remote server and produce an array of command outputs
@@ -53,15 +58,10 @@ class PowerShell(App):
         Output:
             result: A String array of the command outputs
         """
-
-        self.winrm = winrm.Session(self.host,
-                                   auth=(self.username,
-                                         self.device.get_encrypted_field("password")),
-                                   **self.insecure_mode)
         results = []
         status = "Success"
-        for cmd in command:
-            rs = self.winrm.run_cmd(cmd)
+        for command in commands:
+            rs = self.winrm.run_cmd(command)
             if rs.status_code == 0:
                 results.append(rs.std_out)
             else:
@@ -80,7 +80,7 @@ class PowerShell(App):
         return str(results), status
 
     @action
-    def exec_script_remotely(self, local_path, output_filename=None):
+    def exec_script_remotely(self, local_path, output_filename=None, tab_separated_values=False):
         """
         Use Powershell client to execute a script on the remote server and produce an array of command outputs
         Input:
@@ -88,16 +88,26 @@ class PowerShell(App):
         Output:
             result: A String array of the command outputs
         """
-        self.winrm = winrm.Session(self.host,
-                                   auth=(self.username,
-                                         self.device.get_encrypted_field("password")),
-                                   **self.insecure_mode)
         results = []
         try:
             with open(local_path, 'r') as f:
                 script = f.read()
         except IOError as e:
             return e, "FileError"
+
+        # This is because the Kansa scripts are encoded with utf-8-bom
+        en = chardet.detect(script)['encoding']
+        try:
+            script = script.decode(en)
+        except UnicodeDecodeError:
+            return "Could not decode script file.", "UnknownEncoding"
+
+        script = "$tempin = @'\n{}\n'@".format(script)
+        script += ';Invoke-Expression $tempin'
+
+        if tab_separated_values:
+            script += '|ConvertTo-CSV -Delimiter "`t" -NoTypeInformation'
+            script += '|% { $_ -replace "`"" }'
 
         rs = self.winrm.run_ps(script)
         status = "Success"
@@ -112,7 +122,7 @@ class PowerShell(App):
             try:
                 with open(output_filename, 'w') as f:
                     for result in results:
-                        f.write(result)
+                        f.write(result.replace("\r\n", "\n"))
             except IOError as e:
                 return e, "FileError"
 
